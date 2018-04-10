536dc46979aa9c454bb387394868ede0895b2b16
Arpita Hegde <33028166+arpitahegde414@users.noreply.github.com>
2018-04-11 00:27:44 +0530

    Delete concreteCalculator.java

--- monolithic/concreteCalculator.java
+++
@@ -1,8 +0,0 @@
-package monolithic;
-
-public class concreteCalculator implements calculatorInterface {
-	public double add(double a,double b){return a+b;}
-    public double sub(double a,double b){return a-b;}
-    public double mul(double a,double b){return a*b;}
-   public  double div(double a,double b){return a/b;}
-}
