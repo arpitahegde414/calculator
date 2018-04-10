8205b5984baa61a93234f94997bc99bbb083dd53
Arpita Hegde <33028166+arpitahegde414@users.noreply.github.com>
2018-04-11 00:25:22 +0530

    Delete calculatorInterface.java

--- monolithic/calculatorInterface.java
+++
@@ -1,8 +0,0 @@
-package monolithic;
-//Interface
-public interface calculatorInterface {
-    public double add(double a,double b);
-    public double sub(double a,double b);
-    public double mul(double a,double b);
-    public double div(double a,double b);
-}
